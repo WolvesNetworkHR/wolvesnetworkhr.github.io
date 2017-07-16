dd104acec04227e1f7489aa62ab96a8a7f97c749
WolvesNetworkHR <info@wolvesnet.xyz>
2017-07-16 15:15:54 +0200

    Update v1

---
+++ 404.php
@@ -0,0 +1,123 @@
+<link href="https://fonts.googleapis.com/css?family=Raleway:200" rel="stylesheet">
+
+<body bgcolor="#161616">
+
+<style>
+.error {
+    color: #E74C3C;
+}
+.font  {
+	color: #161616;
+	font-family: 'Raleway', sans-serif;
+}
+.center{
+    position:absolute;
+    height: X px;
+    width: Y px;
+    left:36%;
+    top:35%;
+    margin-top:- X/2 px;
+    margin-left:- Y/2 px;
+}
+.file {
+	font-family: 'Raleway', sans-serif;
+	color: #DBDBDB;
+	font-size: 18px;
+}
+hr.line {
+	width: 40%;
+}
+body {
+	background: #161616;
+	color: #161616;
+	font-family: monospace;
+	font-size: 12px;
+	line-height: 12px;		padding:0;	margin:0;
+}
+
+.blink .cursor:before { content: ""; }
+.cursor:before { content: "_"; background: #0f0; color: #0f0; }
+
+#dg-modem {
+	display: block;
+	line-height: 10px;
+	font-size: 10px;
+	font-weight: bold;
+	white-space: pre;
+	font-family: monospace;
+    color: #0f0;
+	background: #161616;
+}marquee {	color: #a2a2a2;		line-height: 15px;		font-size: 15px;		padding:0;	margin:0;}
+a {
+	color: #a2a2a2;
+}
+a:hover {
+	color: #8E8D8D;
+}
+nav > ul,
+nav a {
+	color: #DBDBDB;
+	list-style:none;
+	margin:0;padding:0;
+	text-align:center
+}
+
+nav > ul > li {
+	display:inline-block;
+	margin:0;
+	padding:0;
+	cursor:default
+}
+
+nav > ul > li:after {
+	content:"|";
+	margin:0 8px;
+	opacity:.3
+}
+
+nav > ul > li:last-child:after {
+	content:"";
+	margin:0
+}
+h1 {
+    color: #455c6e;
+    display: block;
+	text-transform: uppercase;
+    margin-top: 30px;
+}
+nav > ul,
+nav a {
+	color: #DBDBDB;
+    font-size: 15px;
+	list-style:none;
+	margin:0;padding:0;
+	text-align:center
+}
+
+nav > ul > li {
+	display:inline-block;
+	margin:0;
+	padding:0;
+	cursor:default
+}
+
+nav > ul > li:after {
+	content:"|";
+	margin:0 8px;
+	opacity:.3
+}
+
+nav > ul > li:last-child:after {
+	content:"";
+
+
+</style>
+
+<title>Oops...</title>
+<body>
+<center>
+<div class="font"><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<h1>Oops! You shouldn't have done that..</h1><br>
+<br>
+<br>
+<marquee width="20%">User attempted to access drwolf.xyz, attempt failed - 404 (Page not Found)</marquee></center>
